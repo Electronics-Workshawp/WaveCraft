52a71b68a600bbdf30bcf6a413654828a59f9c2b
Electronics-Workshawp <115026602+Electronics-Workshawp@users.noreply.github.com>
2023-01-27 06:10:46 -0500

    Update 6OutOfPhaseWaves.ino

--- examples/6OutOfPhaseWaves.ino
+++ examples/6OutOfPhaseWaves.ino
@@ -14,34 +14,34 @@ void setup() {
 
 void loop() {
  
-  wave1.setFrequency(1.5);
+  wave1.setFrequency(.1);
   wave1.setLow(0);
   wave1.setPhase(0);
   wave1.setHigh(255);
 
-  wave2.setFrequency(1.5);
+  wave2.setFrequency(.2);
   wave2.setLow(0);
-  wave2.setPhase(60);
+  wave2.setPhase(0);
   wave2.setHigh(255);
 
-  wave3.setFrequency(1.5);
+  wave3.setFrequency(.3);
   wave3.setLow(0);
-  wave3.setPhase(120);
+  wave3.setPhase(0);
   wave3.setHigh(255);
 
-  wave4.setFrequency(1.5);
+  wave4.setFrequency(.4);
   wave4.setLow(0);
-  wave4.setPhase(180);
+  wave4.setPhase(0);
   wave4.setHigh(255);
 
-  wave5.setFrequency(1.5);
+  wave5.setFrequency(.5);
   wave5.setLow(0);
-  wave5.setPhase(240);
+  wave5.setPhase(0);
   wave5.setHigh(255);
 
-  wave6.setFrequency(1.5);
+  wave6.setFrequency(.6);
   wave6.setLow(0);
-  wave6.setPhase(300);
+  wave6.setPhase(0);
   wave6.setHigh(255);
   
   Serial.print(wave1.getValue());
